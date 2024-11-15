6305cec72443dd6fc1db497e18cac9ed82b54ce1
Aniket Damre <aniketdamre958@gmil.com>
2024-11-15 12:41:17 +0530

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+from .forms import ProductForm
+
+# View to list all products
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+# View to create a new product
+def product_create(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('product_list')
+    return render(request, 'product_form.html', {'form': form})
+
+# View to update an existing product
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid():
+        form.save()
+        return redirect('product_list')
+    return render(request, 'product_form.html', {'form': form})
+
+# View to delete a product
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'product_confirm_delete.html', {'product': product})
